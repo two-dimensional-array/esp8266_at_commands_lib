0439ff6a4bf1a786a35817bd1aba49485a856588
two-dimensional-array <max.mazyr2012@gmail.com>
2021-08-09 00:47:00 +0300

    Class ESP8266_UART is added in source file of esp8266 library

--- lib/esp8266.cpp
+++ lib/esp8266.cpp
@@ -1,1 +1,41 @@
 #include "esp8266.hpp"
+
+// ESP8266_UART
+ESP8266_UART::ESP8266_UART()
+{
+    Init_var();
+    Set_UART_Config(SPEED_115200,BIT_8,STOP_BIT_1,NO_PARITY,NO_FLOW_CONTROL);
+}
+ESP8266_UART::ESP8266_UART(Baud_rate baudrate, Data_bits databits, Stop_bits stopbits, Parity parity, Flow_control flowcontrol)
+{
+    Init_var();
+    Set_UART_Config(baudrate,databits,stopbits,parity,flowcontrol);
+}
+void ESP8266_UART::Set_UART_Config(Baud_rate baudrate, Data_bits databits, Stop_bits stopbits, Parity parity, Flow_control flowcontrol)
+{
+    *this->baudrate = baudrate;
+    *this->databits = databits;
+    *this->stopbits = stopbits;
+    *this->parity = parity;
+    *this->flowcontrol = flowcontrol;
+}
+uint8_t ESP8266_UART::Get_UART_Config(char *buf)
+{
+    return (uint8_t)sprintf(buf, COMMAND_CONFIG_UART, *baudrate, *databits, *stopbits, *parity, *flowcontrol);
+}
+void ESP8266_UART::Init_var()
+{
+    this->baudrate = new Baud_rate;
+    this->databits = new Data_bits;
+    this->stopbits = new Stop_bits;
+    this->parity = new Parity;
+    this->flowcontrol = new Flow_control;
+}
+ESP8266_UART::~ESP8266_UART()
+{
+    delete baudrate;
+    delete databits;
+    delete parity;
+    delete flowcontrol;
+}
+// ESP8266_UART
