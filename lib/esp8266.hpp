1faa7e4d43b5793b27c4f9b2d5853e08c618ac5c
two-dimensional-array <max.mazyr2012@gmail.com>
2021-08-09 00:43:39 +0300

    Class ESP8266_UART is added in header file of esp8266 library

--- lib/esp8266.hpp
+++ lib/esp8266.hpp
@@ -1,4 +1,42 @@
 #include <stdint.h>
+#include <stdio.h>
+
+#define COMMAND_CONFIG_UART "AT+UART_CUR=%i,%i,%i,%i,%i\n\r"                    // baud_rate, data_bits, stop_bits, parity, flow_control
+
+//ESP8266_UART enums
+enum Baud_rate
+{
+    SPEED_4400 = 4400,
+    SPEED_9600 = 9600,
+    SPEED_115200 = 115200,
+    SPEED_4608000 = 4608000
+};
+enum Data_bits
+{
+    BIT_5 = 5,
+    BIT_6 = 6,
+    BIT_7 = 7,
+    BIT_8 = 8
+};
+enum Stop_bits
+{
+    STOP_BIT_1 = 1,
+    STOP_BIT_2 = 3
+};
+enum Parity
+{
+    NO_PARITY = 0,
+    ODD_PARITY = 1,
+    EVEN_PARITY = 2
+};
+enum Flow_control
+{
+    NO_FLOW_CONTROL = 0,
+    RTS_FLOW_CONTROL = 1,
+    CTS_FLOW_CONTROL = 2,
+    RTS_CTS_FLOW_CONTROL = 3
+};
+//ESP8266_UART enums
 
 //base class for ESP8266 modules
 class ESP8266
@@ -17,4 +55,22 @@ protected:
     }
     virtual void Init_var() = 0;
     virtual ~ESP8266(){};
+};
+
+class ESP8266_UART: public ESP8266 
+{
+public:
+    ESP8266_UART();
+    ESP8266_UART(Baud_rate baudrate, Data_bits databits, Stop_bits stopbits, Parity parity, Flow_control flowcontrol);
+    void Set_UART_Config(Baud_rate baudrate, Data_bits databits, Stop_bits stopbits, Parity parity, Flow_control flowcontrol);
+    void Begin() override;
+    ~ESP8266_UART() override;
+private:
+    Baud_rate *baudrate;
+    Data_bits *databits;
+    Stop_bits *stopbits;
+    Parity *parity;
+    Flow_control *flowcontrol;
+    uint8_t Get_UART_Config(char *buf);
+    void Init_var() override;
 };
